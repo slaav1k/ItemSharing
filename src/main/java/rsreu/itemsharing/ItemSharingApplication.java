05df643b3c192dbda87417ff90892918810b0e77
Vyacheslav <slawa.121@yandex.ru>
2025-02-27 20:09:53 +0300

    soed bd

---
+++ src/main/java/rsreu/itemsharing/ItemSharingApplication.java
@@ -0,0 +1,14 @@
+package rsreu.itemsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ItemSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemSharingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rsreu/itemsharing/ItemSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package rsreu.itemsharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemSharingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
