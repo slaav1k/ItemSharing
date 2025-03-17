df0871de043beaa70813f467f6c1ec57940da1d1
Vyacheslav <slawa.121@yandex.ru>
2025-03-17 10:25:13 +0300

    add password

--- createTables.sql
+++ createTables.sql
@@ -17,6 +17,7 @@ CREATE TABLE "users" (
     "full_name" VARCHAR(255) NOT NULL,
     "phone" CHAR(11) NOT NULL,
     "email" VARCHAR(50) NOT NULL,
+	"password" VARCHAR(255) NOT NULL,
     "address" VARCHAR(255) NOT NULL,
     PRIMARY KEY("passport_num")
 );
